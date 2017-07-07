64e59822d6615b5e7f87c957472cc623c3436291
root <root@ari.localdomain>
2017-07-07 12:13:50 +0300

    initalize project

---
+++ src/main/java/com/eteration/StackoverflowCloneApplication.java
@@ -0,0 +1,12 @@
+package com.eteration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowCloneApplication.class, args);
+	}
+}
